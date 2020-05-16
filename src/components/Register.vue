a1546bc3e89634d79ee312fe9746f20b7a7f93c2
gbldyx <gbldyx@yeah.net>
2020-05-16 13:18:55 +0800

    Changed Register component's layout

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-row type="flex" justify="center">
+    <!-- <el-row type="flex" justify="center">
       <el-col :span="6">
         <el-form ref="form" :model="form" :rules="rules" label-position="right" label-width="120px">
             <el-form-item label="姓名" prop="name">
@@ -21,7 +21,29 @@
             </el-form-item>
           </el-form>
       </el-col>
-    </el-row>
+    </el-row> -->
+    <el-container direction="vertical" class="w600-center">
+      <h2>用户注册</h2>
+      <el-form ref="form" :model="form" :rules="rules" label-position="right" label-width="100px">
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="form.email"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input type="password" v-model="form.pass" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkpass">
+          <el-input type="password" v-model="form.checkpass" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">确认</el-button>
+          <el-button type="info" @click="onReturn">返回</el-button>
+          <el-button @click="resetForm('form')">清除</el-button>
+        </el-form-item>
+      </el-form>
+    </el-container>
 </template>
 
 <script>
@@ -94,5 +116,11 @@ export default {
 </script>
 
 <style scoped>
-
+.w600-center {
+  width: 600px;
+  margin: 0 auto;
+}
+h2 {
+  text-align: center;
+}
 </style>
